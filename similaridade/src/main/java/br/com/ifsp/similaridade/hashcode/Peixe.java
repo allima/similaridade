91202d9c151f2b01db4007a914d83fe48c15ae4a
Alexandre de Lima <allima1991@gmail.com>
2015-03-25 02:29:35 -0300

    pe

--- similaridade/src/main/java/br/com/ifsp/similaridade/hashcode/Peixe.java
+++ similaridade/src/main/java/br/com/ifsp/similaridade/hashcode/Peixe.java
@@ -2,4 +2,67 @@ package br.com.ifsp.similaridade.hashcode;
 
 public class Peixe {
 
+	private String ambiente;
+	private String corDorso;
+
+	public Peixe(String ambiente, String corDorso) {
+
+		this.ambiente = ambiente;
+		this.corDorso = corDorso;
+	}
+
+	public String getAmbiente() {
+		return ambiente;
+	}
+
+	public void setAmbiente(String ambiente) {
+		this.ambiente = ambiente;
+	}
+
+	public String getCorDorso() {
+		return corDorso;
+	}
+
+	public void setCorDorso(String corDorso) {
+		this.corDorso = corDorso;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((ambiente == null) ? 0 : ambiente.hashCode());
+		result = prime * result
+				+ ((corDorso == null) ? 0 : corDorso.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Peixe other = (Peixe) obj;
+		if (ambiente == null) {
+			if (other.ambiente != null)
+				return false;
+		} else if (!ambiente.equals(other.ambiente))
+			return false;
+		if (corDorso == null) {
+			if (other.corDorso != null)
+				return false;
+		} else if (!corDorso.equals(other.corDorso))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Peixe [ambiente=" + ambiente + ", corDorso=" + corDorso + "]";
+	}
+
 }
--- similaridade/src/main/java/br/com/ifsp/similaridade/hashcode/Tubarao.java
+++ similaridade/src/main/java/br/com/ifsp/similaridade/hashcode/Tubarao.java
@@ -2,4 +2,44 @@ package br.com.ifsp.similaridade.hashcode;
 
 public class Tubarao extends Peixe {
 
+	private String tipo;
+
+	public Tubarao(String ambiente, String corDorso, String tipo) {
+		super(ambiente, corDorso);
+		this.tipo = tipo;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((tipo == null) ? 0 : tipo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tubarao other = (Tubarao) obj;
+		if (tipo == null) {
+			if (other.tipo != null)
+				return false;
+		} else if (!tipo.equals(other.tipo))
+			return false;
+		return true;
+	}
+
 }
