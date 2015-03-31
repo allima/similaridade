d727f5d636d0483573a2d06eff3967f15f492fb6
Alexandre de Lima <allima1991@gmail.com>
2015-03-31 14:19:12 -0300

    Update ArquivoDoc.java
    .

--- similaridade/src/main/java/br/com/ifsp/similaridade/modelo/ArquivoDoc.java
+++ similaridade/src/main/java/br/com/ifsp/similaridade/modelo/ArquivoDoc.java
@@ -9,7 +9,46 @@ public class ArquivoDoc extends Arquivo {
 
 	@Override
 	public void lerArquivo() {
-		// TODO Auto-generated method stub
+		try {
+			//só doc não docx 
+			POIFSFileSystem poifs = new POIFSFileSystem(new FileInputStream(this.getCaminho()));  
+    			WordExtractor extractor = new WordExtractor(poifs);  
+			String wordText = extractor.getText();
+			
+		//	BufferedReader lerArq = new BufferedReader(new InputStreamReader(
+		//			new FileInputStream(this.getCaminho()), "UTF-8"));
+		//	String linha = lerArq.readLine();
+
+		//	while (linha != null) {
+				wordText = Normalizer.normalize(linha, Normalizer.Form.NFD);
+				wordText = wordText.replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase();
+				// System.out.println(linha);
+				Pattern p = Pattern.compile("(\\d+)|([a-z]+)");
+				Matcher m = p.matcher(wordText);
+
+				while (m.find()) {
+					String palavra = m.group();
+					Integer freq = getMapa().get(palavra);
+					if (freq != null) {
+						getMapa().put(palavra, freq + 1);
+					} else {
+						getMapa().put(palavra, 1);
+					}
+				}
+
+			//	linha = lerArq.readLine();
+			//}
+			for (Entry<String, Integer> entry : getMapa().entrySet()) {
+				System.out.println(entry.getKey());
+				System.out.println("--> freq=" + entry.getValue() + "\n");
+			}
+
+			lerArq.close();
+			// arq.close();
+		} catch (IOException e) {
+			System.err
+					.println("Erro na abertura do arquivo: " + e.getMessage());
+		}
 		
 	}
 
