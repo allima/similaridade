1a33d6fbb7046479de6ebf0332d6cb188efbd9df
Alexandre de Lima <allima1991@gmail.com>
2015-04-01 00:48:16 -0300

    ler doc

--- similaridade/src/main/java/br/com/ifsp/similaridade/modelo/ArquivoDoc.java
+++ similaridade/src/main/java/br/com/ifsp/similaridade/modelo/ArquivoDoc.java
@@ -1,5 +1,15 @@
 package br.com.ifsp.similaridade.modelo;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.Normalizer;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+
 public class ArquivoDoc extends Arquivo {
 
 	public ArquivoDoc(String caminho) {
@@ -10,52 +20,53 @@ public class ArquivoDoc extends Arquivo {
 	@Override
 	public void lerArquivo() {
 		try {
-			//s√≥ doc n√£o docx 
-			POIFSFileSystem poifs = new POIFSFileSystem(new FileInputStream(this.getCaminho()));  
-    			WordExtractor extractor = new WordExtractor(poifs);  
+			// s„o doc n„o docx
+			POIFSFileSystem poifs = new POIFSFileSystem(new FileInputStream(
+					this.getCaminho()));
+			WordExtractor extractor = new WordExtractor(poifs);
 			String wordText = extractor.getText();
-			
-		//	BufferedReader lerArq = new BufferedReader(new InputStreamReader(
-		//			new FileInputStream(this.getCaminho()), "UTF-8"));
-		//	String linha = lerArq.readLine();
 
-		//	while (linha != null) {
-				wordText = Normalizer.normalize(linha, Normalizer.Form.NFD);
-				wordText = wordText.replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase();
-				// System.out.println(linha);
-				Pattern p = Pattern.compile("(\\d+)|([a-z]+)");
-				Matcher m = p.matcher(wordText);
+			// BufferedReader lerArq = new BufferedReader(new InputStreamReader(
+			// new FileInputStream(this.getCaminho()), "UTF-8"));
+			// String linha = lerArq.readLine();
 
-				while (m.find()) {
-					String palavra = m.group();
-					Integer freq = getMapa().get(palavra);
-					if (freq != null) {
-						getMapa().put(palavra, freq + 1);
-					} else {
-						getMapa().put(palavra, 1);
-					}
+			// while (linha != null) {
+			wordText = Normalizer.normalize(wordText, Normalizer.Form.NFD);
+			wordText = wordText.replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase();
+			// System.out.println(linha);
+			Pattern p = Pattern.compile("(\\d+)|([a-z]+)");
+			Matcher m = p.matcher(wordText);
+
+			while (m.find()) {
+				String palavra = m.group();
+				Integer freq = getMapa().get(palavra);
+				if (freq != null) {
+					getMapa().put(palavra, freq + 1);
+				} else {
+					getMapa().put(palavra, 1);
 				}
+			}
 
-			//	linha = lerArq.readLine();
-			//}
+			// linha = lerArq.readLine();
+			// }
 			for (Entry<String, Integer> entry : getMapa().entrySet()) {
 				System.out.println(entry.getKey());
 				System.out.println("--> freq=" + entry.getValue() + "\n");
 			}
 
-			lerArq.close();
+			// lerArq.close();
 			// arq.close();
 		} catch (IOException e) {
 			System.err
 					.println("Erro na abertura do arquivo: " + e.getMessage());
 		}
-		
+
 	}
 
 	@Override
 	public void PreecheMapa(String[] palavra) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
